ff0af1be2ad446a12a995bb81be4a866acc199c9
Allen <arusman1987@gmail.com>
2023-08-30 14:26:56 +0800

    dfsdf

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return ( 
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2023 Allen's Store Inc. All Rights Reserved
+                </p>
+            </div>
+        
+        </footer>
+     );
+}
+ 
+export default Footer;
+<footer>
+    footer
+</footer>
---
+++ components/ui/no-result.tsx
@@ -0,0 +1,15 @@
+const NoResults = () => {
+    return ( 
+        <div className="
+            flex items-center
+            justify-center
+            h-full
+            w-full
+            texy-neutral
+        ">
+            No results found
+        </div>
+     );
+}
+ 
+export default NoResults;
